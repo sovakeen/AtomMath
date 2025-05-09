646f1871347dbb8fd41bc83eea2f46d61331fe38
andrew <rabykin2005@gmail.com>
2025-05-10 00:41:35 +0300

    fixed issue with modify_term (after "id" field deletion view still requested entered id)

--- summary_app/views.py
+++ summary_app/views.py
@@ -15,38 +15,38 @@ from .models import Term
 
 
 def index(request):
-    term_list = Term.objects.order_by("id")
-    shorts = {"Term": "Def", "Theorem": "The", "Article": "Art", "Undefined": "Und", "Example": "Exp"}
-    return render(request, "summary_app/index.html", { "term_list": term_list, "shorts": shorts })
+    term_list = Term.objects.order_by('id')
+    shorts = {'Term': 'Def', 'Theorem': 'The', 'Article': 'Art', 'Undefined': 'Und', 'Example': 'Exp'}
+    return render(request, 'summary_app/index.html', { 'term_list': term_list, 'shorts': shorts })
 
 
 def add(request):
-    return render(request, "summary_app/add.html", { "term_types": Term.Type.labels })
+    return render(request, 'summary_app/add.html', { 'term_types': Term.Type.labels })
 
 
 def read(request, term_id):
     term = get_object_or_404(Term, pk=term_id)
-    prev = Term.objects.filter(id__lt=term_id).order_by("id").last()
-    next = Term.objects.filter(id__gt=term_id).order_by("id").first()
-    return render(request, "summary_app/read.html", { "term": term, "next": next, "prev": prev })
+    prev = Term.objects.filter(id__lt=term_id).order_by('id').last()
+    next = Term.objects.filter(id__gt=term_id).order_by('id').first()
+    return render(request, 'summary_app/read.html', { 'term': term, 'next': next, 'prev': prev })
 
 
 def edit(request, term_id):
     term = get_object_or_404(Term, pk=term_id)
-    return render(request, "summary_app/edit.html", { "term": term, "term_types": Term.Type.labels })
+    return render(request, 'summary_app/edit.html', { 'term': term, 'term_types': Term.Type.labels })
 
 
 def sequential(request):
-    term_list = Term.objects.order_by("id")
-    return render(request, "summary_app/sequential.html", { "term_list": term_list })
+    term_list = Term.objects.order_by('id')
+    return render(request, 'summary_app/sequential.html', { 'term_list': term_list })
 
 
 def test(request, data={}):
-    return render(request, "summary_app/test.html", { "result": data })
+    return render(request, 'summary_app/test.html', { 'result': data })
 
 
 def backup(request):
-    return render(request, "summary_app/backup.html")
+    return render(request, 'summary_app/backup.html')
 
 
 # functions listed below are not actually views representing pages but request processors
@@ -56,10 +56,10 @@ def add_term(request):
     try:
         term = Term(name=request.POST['term_name'], definition=request.POST['term_definition'], type=request.POST['term_type'])
     except KeyError:
-        return render(request, "summary_app/add.html", { "error_message": "KEY ERROR" })
+        return render(request, 'summary_app/add.html', { 'error_message': 'KEY ERROR' })
     else:
         term.save()
-        return redirect("summary_app:add")
+        return redirect('summary_app:add')
     
 
 # why redirect is not working (mb problem with args form)?
@@ -68,47 +68,41 @@ def modify_term(request, term_id):
     term.name = request.POST['term_name']
     term.definition = request.POST['term_definition']
     term.type = request.POST['term_type']
-    entered_id = request.POST['term_id']
-    if str(term_id) == entered_id or not Term.objects.filter(pk=entered_id).exists():
-        term.pk = entered_id
-        Term.objects.filter(pk=term_id).delete()
-        term.save()
-        return HttpResponseRedirect(reverse("summary_app:read", args=(entered_id,)))
-    else:
-        return redirect(reverse("summary_app:edit", args=[term_id]) + "?error=id_exists")
+    term.save()
+    return redirect(reverse('summary_app:read', args=[term_id]))
 
 
 def delete_all(request):
     Term.objects.all().delete()
-    return redirect("summary_app:index")
+    return redirect('summary_app:index')
 
 
 def delete_term(request, term_id):
     term = get_object_or_404(Term, pk=term_id)
     term.delete()
-    return redirect("summary_app:index")
+    return redirect('summary_app:index')
 
 
 # doesn't redirect, so url remains .../exec_sql/
 def execute_raw_sql(request):
-    sql_query = request.POST["sql_query"]
+    sql_query = request.POST['sql_query']
     res = Term.objects.raw(sql_query)
-    return render(request, "summary_app/test.html", { "result": res, "res_dict": res.__dict__, "item_dict": res[0].__dict__ }) # add/rem "item_dict" makes diff
+    return render(request, 'summary_app/test.html', { 'result': res, 'res_dict': res.__dict__, 'item_dict': res[0].__dict__ }) # add/rem 'item_dict' makes diff
     # return test(request, res)
-    # return redirect("summary_app:test")
+    # return redirect('summary_app:test')
 
 
 def send_backup(request):
     email = EmailMessage(
-        request.POST["mail_topic"],
-        "Sending data backup for " + str(datetime.now()) + ". Provided text comment:\n" + request.POST["mail_text"],
-        "from@example.com",
-        ["rabykin2005@gmail.com"],
+        request.POST['mail_topic'],
+        'Sending data backup for ' + str(datetime.now()) + '. Provided text comment:\n' + request.POST['mail_text'],
+        'from@example.com',
+        ['rabykin2005@gmail.com'],
         []
     )
     email.attach_file(Path(__file__).parent.parent.absolute().joinpath('db.sqlite3'))
     email.send(fail_silently=False)
-    return redirect("summary_app:index")
+    return redirect('summary_app:index')
 
 
 # to deprecate or to replace csv with json
@@ -123,7 +117,7 @@ def prepopulate(request):
                 definition=row['definition'],
                 type=row['type']
             )
-    return redirect("summary_app:index")
+    return redirect('summary_app:index')
 
 
 @csrf_exempt
@@ -151,7 +145,7 @@ def export_terms(request):
         # create http response with json content
         response = HttpResponse(serialized_data, content_type='application/json')
         # trigger download with a default filename
-        response['Content-Disposition'] = 'attachment; filename=\"terms.json\"'
+        response['Content-Disposition'] = 'attachment; filename=\'terms.json\''
         return response
     except Exception as e:
         messages.error(request, f'Error exporting: {str(e)}')
